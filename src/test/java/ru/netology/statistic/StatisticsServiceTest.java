ddb15f5f6f66041317f827d9faf58f89b73cb11d
Moks <gutamax2@mail.ru>
2022-01-08 19:57:40 +0500

    added test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+  @Test
+  void findMax1() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 22, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 22;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
 }
